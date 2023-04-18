82c41ff77d9c22531c8de5e277041cb4118ed3e4
Heisenbug <58136242+HeisenbugZ@users.noreply.github.com>
2023-04-17 17:18:20 -0700

    improve header syntax

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -6,28 +6,34 @@ import { Link } from 'react-router-dom'
 function Header() {
   return (
     <header className='headerContainer'>
-      {/* <div className='headerContainer'> */}
-        <div className='logo'>
-          <Link to="/">
-            <img src={logo} alt='logo'style={{
-                      width:"100%",
-                      height:"100%"
-                      }}/>
-          </Link>
-        </div>
-        <div>
-            <h1 className='mainHeader'>Los Angeles Business Heat Map &amp; Industry Selection Recommendation</h1>
-            {/* <p>Some description about the application.</p> */}
-        </div>
-        <div className='dataSource'>
-          <p>Data Source : 
-          <a target="_blank" href="https://data.lacity.org/Administration-Finance/Listing-of-All-Businesses/r4uk-afju" rel="noreferrer"> LOS ANGELES OPEN DATA</a>
-          </p>
-          <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <a target="_blank" href="https://www.bls.gov/cew/downloadable-data-files.htm" rel="noreferrer"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U.S. BUREAU OF LABOR STATISTICS</a>
-          </p>
-        </div>
-      {/* </div> */}
+      <div className='logo'>
+        <Link to="/">
+          <img src={logo}
+               alt='logo'
+               style={{
+                  width:"100%",
+                  height:"100%"
+                }}/>
+        </Link>
+      </div>
+      <div>
+          <h1 className='mainHeader'>Los Angeles Business Heat Map &amp; Industry Selection Recommendation</h1>
+      </div>
+      <div className='dataSource'>
+
+        <p>Data Source:  
+          <a href="https://data.lacity.org/Administration-Finance/Listing-of-All-Businesses/r4uk-afju" > 
+            LOS ANGELES OPEN DATA
+          </a>
+        </p>
+
+        <p style={{ marginLeft: '72px', display: 'inline-block' }}>
+          <a href="https://www.bls.gov/cew/downloadable-data-files.htm" >
+            U.S. BUREAU OF LABOR STATISTICS
+          </a>
+        </p>
+        
+      </div>
     </header>
   );
 }
