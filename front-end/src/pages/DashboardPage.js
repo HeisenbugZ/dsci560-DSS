68776bf907ac1565c394306129eb786d830ce93f
Heisenbug <58136242+HeisenbugZ@users.noreply.github.com>
2023-04-18 18:12:13 -0700

    feature: Dashboard header
    1. go back button
    2. district selector
    3. industry selector

--- front-end/src/pages/DashboardPage.js
+++ front-end/src/pages/DashboardPage.js
@@ -1,37 +1,41 @@
 import React, { useState } from 'react';
 import Capacity from '../components/Capacity';
-import Header from '../components/Header';
+import DashboardHeader from '../components/DashboardHeader';
 import '../styles/Detail.css';
 
 
 export default function DashboardPage( {selectedDistrict, setSelectedDistrict} ){
   const [selectIndustry, setSelecteIndustry] = useState("all")
+
   return (
     <div className='detail-container'>
+
       <div className='detail-header'>
-          <Header />
+          <DashboardHeader
+            district={ selectedDistrict }
+            setDistrict={ setSelectedDistrict }
+            industry={ selectIndustry }
+            setIndustry={ setSelecteIndustry }/>
       </div>
+
       <div className='detail-content'>
       
-          <div className='left-container'>
-          <Capacity
-            district={ selectedDistrict }
-            industry={ selectIndustry }
-          />
-          </div >
-          <div className='middle-container'>
-          {/* <InteractiveMap
-              selectedDistrict={selectedDistrict}
-              setSelectedDistrict={setSelectedDistrict}
-          />
-          <SideDashboard 
-              selectedDistrict={selectedDistrict}
-          /> */}
-          </div>
-          <div className='right-container'>
+        <div className='left-container'>
+        <Capacity
+          district={ selectedDistrict }
+          industry={ selectIndustry }
+        />
+        </div >
 
-          </div>
+        <div className='middle-container'>
+        </div>
+
+
+        <div className='right-container'>
+        </div>
+
       </div>
+
     </div>
   )
 }
