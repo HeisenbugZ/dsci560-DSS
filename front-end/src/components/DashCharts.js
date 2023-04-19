093a3abd9ba357ecf8371d4a3485f43e046c151c
Heisenbug <58136242+HeisenbugZ@users.noreply.github.com>
2023-04-19 01:54:44 -0700

    feature: DashChart basic function

--- front-end/src/components/DashCharts.js
+++ front-end/src/components/DashCharts.js
@@ -2,100 +2,100 @@ import React, { useEffect, useRef }from 'react';
 // import ReactEcharts from "echarts-for-react"
 import * as echarts from "echarts";
 import { Card } from 'antd';
-// import axios from 'axios';
-// import { API_Change_Ratio } from '../utils/APIs';
+import axios from 'axios';
+import { API_Active_Business } from '../utils/APIs';
 import '../styles/Dashboard.css'
 
-export default function DashCharts({ district }) {
+function createSeriesList(n) {
+  const series = [];
+  for (let i = 0; i < n; i++) {
+    series.push({
+      type: 'line',
+      smooth: true,
+      seriesLayoutBy: 'row',
+      emphasis: { focus: 'series' }
+    });
+  }
+  series.push({
+    type: 'pie',
+    id: 'pie',
+    radius: '30%',
+    center: ['50%', '25%'],
+    emphasis: { focus: 'self' },
+    label: { formatter: '{b}: {@2022/2} ({d}%)' },
+    encode: { itemName: 'Industry', value: '2022/2', tooltip: '2022/2' }
+  });
 
+  return series
+}
+
+export default function DashCharts({ district }) {
   const chartRef = useRef(null);
   useEffect(() => {
-    let chartInstance = echarts.init(chartRef.current);
-    const option = {
-      legend: {},
-      tooltip: {
-        trigger: 'axis',
-        showContent: false
-      },
-      dataset: {
-        source: [
-          ['product', '2012', '2013', '2014', '2015', '2016', '2017'],
-          ['Milk Tea', 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
-          ['Matcha Latte', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
-          ['Cheese Cocoa', 40.1, 62.2, 69.5, 36.4, 45.2, 32.5],
-          ['Walnut Brownie', 25.2, 37.1, 41.2, 18, 33.9, 49.1]
-        ]
-      },
-      xAxis: { type: 'category' },
-      yAxis: { gridIndex: 0 },
-      grid: { top: '55%' },
-      series: [
-        {
-          type: 'line',
-          smooth: true,
-          seriesLayoutBy: 'row',
-          emphasis: { focus: 'series' }
-        },
-        {
-          type: 'line',
-          smooth: true,
-          seriesLayoutBy: 'row',
-          emphasis: { focus: 'series' }
-        },
-        {
-          type: 'line',
-          smooth: true,
-          seriesLayoutBy: 'row',
-          emphasis: { focus: 'series' }
-        },
-        {
-          type: 'line',
-          smooth: true,
-          seriesLayoutBy: 'row',
-          emphasis: { focus: 'series' }
-        },
-        {
-          type: 'pie',
-          id: 'pie',
-          radius: '30%',
-          center: ['50%', '25%'],
-          emphasis: {
-            focus: 'self'
+    const current = chartRef.current;
+    const chartInit = () => {
+      let chartInstance = echarts.init(chartRef.current);
+      axios.get(API_Active_Business(district,null,null,10)).then(res => {
+        const data = res.data
+        const formattedData = [
+          ['Industry', ...data.time],
+          ...data.industries.map(industry => [
+            industry.name.trim(),
+            ...industry.active
+          ])
+        ];
+        // console.log(formattedData)
+        const option = {
+          legend: {
+            type: 'scroll',
+            // orient: 'vertical',
+            data: data.industries.map(item => item.name)
           },
-          label: {
-            formatter: '{b}: {@2012} ({d}%)'
+          tooltip: [
+            // {trigger: 'item',
+            // formatter: '{a} <br/>{b} : {c} ({d}%)'},
+            {trigger: 'axis',
+            showContent: false}
+          ],
+          dataset: {
+            source: formattedData
           },
-          encode: {
-            itemName: 'product',
-            value: '2012',
-            tooltip: '2012'
-          }
-        }
-      ]
-    };
-    chartInstance.on('updateAxisPointer', function (event) {
-      const xAxisInfo = event.axesInfo[0];
-      if (xAxisInfo) {
-        const dimension = xAxisInfo.value + 1;
-        chartInstance.setOption({
-          series: {
-            id: 'pie',
-            label: {
-              formatter: '{b}: {@[' + dimension + ']} ({d}%)'
-            },
-            encode: {
-              value: dimension,
-              tooltip: dimension
-            }
+          xAxis: { type: 'category' },
+          yAxis: { gridIndex: 0 },
+          grid: { top: '55%' },
+          series: createSeriesList(32)
+        };
+        chartInstance.on('updateAxisPointer', function (event) {
+          const xAxisInfo = event.axesInfo[0];
+          if (xAxisInfo) {
+            const dimension = xAxisInfo.value + 1;
+            chartInstance.setOption({
+              series: {
+                id: 'pie',
+                label: {
+                  formatter: '{b}: {@[' + dimension + ']} ({d}%)'
+                },
+                encode: {
+                  value: dimension,
+                  tooltip: dimension
+                }
+              }
+            });
           }
         });
-      }
-    });
+        chartInstance.setOption(option);
+      })
+      
+      
 
-    chartInstance.setOption(option);
-
+      
+    }
+    chartInit();
+    return () => {
+      echarts.dispose(current);
+    };
     
-  }, [])
+  }, [district])
   
 
   return (
