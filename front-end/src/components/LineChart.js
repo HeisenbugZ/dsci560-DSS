71d30100a1208a9a181479427cde572e032e9e27
Heisenbug <58136242+HeisenbugZ@users.noreply.github.com>
2023-04-19 00:56:41 -0700

    change chart library
    "echarts-for-react" to "echart"

--- front-end/src/components/LineChart.js
+++ front-end/src/components/LineChart.js
@@ -1,76 +1,83 @@
-import React, { useState, useEffect }from 'react';
-import ReactEcharts from "echarts-for-react"
+import React, { useEffect, useRef }from 'react';
+// import ReactEcharts from "echarts-for-react"
+import * as echarts from "echarts";
 import axios from 'axios';
 import { API_Change_Ratio } from '../utils/APIs';
 import '../styles/Sider.css'
 
 function LineChart({ district }) {
 
-  const [option, setOption] = useState({});
+  const chartRef = useRef(null);
 
   useEffect(() => {
-    axios.get(API_Change_Ratio(district, '2020',null, 10)).then(res => {
-      // console.log(res);
-      const data = res.data
-      // console.log(data.time)
-      const newOption = {
-        grid: { top: 40, right: 40, bottom: 30, left: 60 },
-        title: {
-          text: 'Active Business Quarterly Growth Rate during Periods',
-          // subtext: '',
-          left: 'center',
-          textStyle: {
-            fontSize: 16,
-            fontStyle: "normal",
-            fontWeight: "lighter",
-            fontFamily: "Arial",
-            color: "rgba(0, 0, 0, 0.59)",
+    const current = chartRef.current;
+    const chartInit = () => {
+      let chartInstance = echarts.init(current);
+      axios.get(API_Change_Ratio(district, '2020',null, 10)).then(res => {
+        // console.log(res);
+        const data = res.data
+        // console.log(data.time)
+        const option = {
+          grid: { top: 40, right: 40, bottom: 30, left: 60 },
+          title: {
+            text: 'Active Business Quarterly Growth Rate during Periods',
+            // subtext: '',
+            left: 'center',
+            textStyle: {
+              fontSize: 16,
+              fontStyle: "normal",
+              fontWeight: "lighter",
+              fontFamily: "Arial",
+              color: "rgba(0, 0, 0, 0.59)",
+            },
+            padding:[10,10,10,10],
           },
-          padding:[10,10,10,10],
-        },
-        
-        xAxis: {
-          type: "category",
-          data: data.time
-        },
-        yAxis: {
-          type: "value",
-          axisLabel:{
-            formatter: "{value} %"
-          }
-        },
-        tooltip: {
-          trigger: 'axis',
-          valueFormatter: (value) => {
-            if (value !== undefined){
-              return value.toFixed(4) + '%'
-            }else{
-              return value
+          
+          xAxis: {
+            type: "category",
+            data: data.time
+          },
+          yAxis: {
+            type: "value",
+            axisLabel:{
+              formatter: "{value} %"
             }
-          }
-          // formatter: '{a} <br/>{b} : {c} ({d}%)'
-        },
-        series: data.industries.map( industry => {
-          return {
-            name: industry.name,
-            data: industry.change_rate,
-            type: "line",
-            smooth: true
-          }
-        })
-      }
-      setOption(newOption)
-    })
-      
-
+          },
+          tooltip: {
+            trigger: 'axis',
+            valueFormatter: (value) => {
+              if (value !== undefined){
+                return value.toFixed(4) + '%'
+              }else{
+                return value
+              }
+            }
+            // formatter: '{a} <br/>{b} : {c} ({d}%)'
+          },
+          series: data.industries.map( industry => {
+            return {
+              name: industry.name,
+              data: industry.change_rate,
+              type: "line",
+              smooth: true
+            }
+          })
+        }
+        chartInstance.setOption(option);
+      });
+    }
+    chartInit();
+    return () => {
+      echarts.dispose(current);
+    };
+    
   }, [district])
+
   return (
     <div className='lineChart'>
-      {/* <h3>Line Chart for {`District ${district}`}</h3> */}
-      <ReactEcharts
-        option={option}
-        style={{ width: "100%", height: "100%" }}
-      ></ReactEcharts>
+      <div ref={chartRef} 
+           style={{ width: "100%", height: "100%" }}>
+      </div>
     </div>
   );
 }
--- front-end/src/components/PieChart.js
+++ front-end/src/components/PieChart.js
@@ -1,53 +1,63 @@
-import React, { useState, useEffect }from 'react';
-import ReactEcharts from "echarts-for-react"
+import React, { useRef, useEffect }from 'react';
+// import ReactEcharts from "echarts-for-react"
+import * as echarts from "echarts";
 import axios from 'axios';
 import { API_Active_Business } from '../utils/APIs';
 import '../styles/Sider.css'
 
 function PieChart({ district }) {
-  const [option, setOption] = useState({});
+  const chartRef = useRef(null);
+
   useEffect(() => {
-    axios.get(API_Active_Business(district,null,'2022',10)).then(res => {
-      // console.log(res);
-      const data = res.data
-      const pie_data = data.industries.map( item => {
-        return {
-          name: item.name,
-          value: item.active.slice(-1)[0],
+    const current = chartRef.current;
+    const chartInit = () => {
+      let chartInstance = echarts.init(current);
+      axios.get(API_Active_Business(district,null,'2022',10)).then(res => {
+        // console.log(res);
+        const data = res.data
+        const pie_data = data.industries.map( item => {
+          return {
+            name: item.name,
+            value: item.active.slice(-1)[0],
+          }
+        })
+        const option = {
+          grid: { top: 20, right: 40, bottom: 10, left: 40 },
+          // legend: {
+          //   type: 'scroll',
+          //   orient: 'vertical',
+          //   right: 10,
+          //   top: 20,
+          //   bottom: 20,
+          //   data: pie_data.map( i => {
+          //     return i.name
+          //   })
+          // },
+          tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b} : {c} ({d}%)'
+          },
+          series: {
+            name: "Industry",
+            data: pie_data,
+            type: "pie",
+            smooth: true
+          }
         }
+        chartInstance.setOption(option);
       })
-      const newOption = {
-        grid: { top: 20, right: 40, bottom: 10, left: 40 },
-        // legend: {
-        //   type: 'scroll',
-        //   orient: 'vertical',
-        //   right: 10,
-        //   top: 20,
-        //   bottom: 20,
-        //   data: pie_data.map( i => {
-        //     return i.name
-        //   })
-        // },
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)'
-        },
-        series: {
-          name: "Industry",
-          data: pie_data,
-          type: "pie",
-          smooth: true
-        }
-      }
-      setOption(newOption)
-    })
+    }
+    chartInit();
+    return () => {
+      echarts.dispose(current);
+    };
   }, [district])
   
   return (
     <div className='pieChart'>
-      <ReactEcharts
-        option={option}
-      ></ReactEcharts>
+      <div ref={chartRef} 
+           style={{ width: "100%", height: "100%" }}>
+      </div>
     </div>
   );
 }
