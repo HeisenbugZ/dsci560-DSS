f9b87d586e215858b900c24eefa9f4b0e6bf1795
Heisenbug <58136242+HeisenbugZ@users.noreply.github.com>
2023-04-24 03:49:18 -0700

    feature: chatbot practice

---
+++ front-end/src/components/Chatbot.js
@@ -0,0 +1,77 @@
+import Chatbot from 'react-chatbot-kit'
+import 'react-chatbot-kit/build/main.css'
+import config from './ChatbotComp/config.js';
+import MessageParser from './ChatbotComp/MessageParser.js';
+import ActionProvider from './ChatbotComp/ActionProvider.js';
+
+export default function Chatbott () {
+  return (
+    <div>
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
+    </div>
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import ChatBot from 'react-simple-chatbot';
+// import { ThemeProvider } from 'styled-components';
+
+// const theme = {
+//   background: '#f5f8fb',
+//   fontFamily: 'Helvetica Neue',
+//   headerBgColor: '#EF6C00',
+//   headerFontColor: '#fff',
+//   headerFontSize: '15px',
+//   botBubbleColor: '#EF6C00',
+//   botFontColor: '#fff',
+//   userBubbleColor: '#fff',
+//   userFontColor: '#4a4a4a',
+// };
+
+
+// export default function Chatbot () {
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <ChatBot
+//         headerTitle="Speech Recognition"
+//         recognitionEnable={true}
+//         steps={[
+//           {
+//             id: '1',
+//             message: 'What is your name?',
+//             trigger: '2',
+//           },
+//           {
+//             id: '2',
+//             user: true,
+//             trigger: '3',
+//           },
+//           {
+//             id: '3',
+//             message: 'Hi {previousValue}, nice to meet you!',
+//             trigger: '2',
+//           },
+//         ]}
+//       />
+//     </ThemeProvider>
+//   )
+// }
